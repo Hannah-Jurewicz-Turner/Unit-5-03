25696192efe1101085d5d2f0839dda706c1ab67c
HannahJT <>
2021-06-02 13:10:39 +0000

    Added spaces to java.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // In this code, whatever the user types into the input box gets displayed as an alert when the button is clicked
-document.getElementById('report').addEventListener('click',reportResults)
+document.getElementById('report').addEventListener('click', reportResults)
 
 function reportResults () {
   alert(document.getElementById('input').value)
